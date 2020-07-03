9defb8e9984618afae9a83bc7185291b63ad8f7c
Otis Simon <osimon@utilitywarehouse.co.uk>
2020-07-03 14:25:22 +0100

    feat: create new nextjs App

--- src/components/UI/Spinner/Spinner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import classes from './Spinner.css';
-
-const Spinner = () => (
-  <div className={classes.Spinner}></div>
-)
-
-export default Spinner;
--- src/store/actions/index.js
+++
@@ -1,2 +0,0 @@
-export { getPocketToken, checkAuthenticated } from './auth'
-export { updateStats, getStats, getHomepage } from './stats'
